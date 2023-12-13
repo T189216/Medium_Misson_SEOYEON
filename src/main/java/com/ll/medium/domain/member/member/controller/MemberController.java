ac2b534f22815b2ac189f0f546212f2230742348
seoyeon <tus0926@naver.com>
2023-12-13 16:51:29 +0900

    joinForm.java

--- src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
+++ src/main/java/com/ll/medium/domain/member/member/controller/MemberController.java
@@ -1,8 +1,10 @@
 package com.ll.medium.domain.member.member.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +14,11 @@ public class MemberController {
     @GetMapping("/join")
     public String showJoin() {
         return "domain/member/member/join";
+    }
+
+    @PostMapping("/join")
+    public String join(@Valid JoinForm joinForm) {
+
+        return "redirect:/";
     }
 }
