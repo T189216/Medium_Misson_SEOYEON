a41c240f2a5d4342c7cc1a2c3f93e007f9c1fd34
seoyeon <tus0926@naver.com>
2024-01-03 12:29:14 +0900

    QueryDSL 도입

--- src/main/java/com/ll/medium/domain/post/post/service/PostService.java
+++ src/main/java/com/ll/medium/domain/post/post/service/PostService.java
@@ -42,11 +42,11 @@ public class PostService {
     }
 
     public Page<Post> search(String kw, Pageable pageable) {
-        return postRepository.findByIsPublishedAndTitleContainingIgnoreCaseOrIsPublishedAndBodyContainingIgnoreCase(true, kw, true, kw, pageable);
+        return postRepository.search(true, kw, pageable);
     }
 
     public Page<Post> search(Member author, String kw, Pageable pageable) {
-        return postRepository.findByAuthorAndTitleContainingIgnoreCaseOrAuthorAndBodyContainingIgnoreCase(author, kw, author, kw, pageable);
+        return postRepository.search(author, kw, pageable);
     }
 
     public boolean canModify(Member author, Post post) {
